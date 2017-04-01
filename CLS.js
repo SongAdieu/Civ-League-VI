84b42b628ed923bfaaf47b4d57466cf049b517c7
Alexander Blair <alex@snipanet.com>
2017-04-01 00:49:11 -0700

    Update with new ignore block + add package.json + clean up code.

--- CLS.js
+++ CLS.js
@@ -1,3 +1,4 @@
+"use strict";
 /*Author: © • Song Adieu | Created: March, 2017*/
 
 const Discord = require("discord.js");
@@ -5,21 +6,21 @@ const Players = require("./players.json");
 
 const CivLeagueStats = new Discord.Client();
 
-CivLeagueHelp.on("ready", () => {
-  console.log("*CivLeagueStats Activated*");
+CivLeagueStats.on("ready", () => {
+    console.log("*CivLeagueStats Activated*");
 })
 
 CivLeagueStats.on("message", message => {
-  if (message.author.CivLeagueStats) return;
-  if (!message.content.startsWith(Config.dot)) return;
+    if (message.author.CivLeagueStats) return;
+    if (!message.content.startsWith(Config.dot)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(Config.dot.length);
-  let args = message.content.split(" ").splice(1);
+    let command = message.content.split(" ")[0];
+    command = command.slice(Config.dot.length);
+    let args = message.content.split(" ").splice(1);
 
-  //Song Adieu
-  if(command === "SongAdieu") {
-    
-  }
+    //Song Adieu
+    if (command === "SongAdieu") {
+
+    }
 });
 
